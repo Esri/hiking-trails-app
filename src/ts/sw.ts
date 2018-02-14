237adeed2c5814b27cba7335d445230a4033ed5c
rnicola@esri.com <rnicola@esri.com>
2018-02-14 14:44:09 +0100

    Cache the api, basemap and the ground in the SW

--- src/ts/sw.ts
+++ src/ts/sw.ts
@@ -11,10 +11,9 @@ function generateAssets(): Asset[] {
 
   generateAppResources(assets);
   generateRootResources(assets);
-  // generateAPIResources(assets);
-  // generatePortalResources(assets);
-  // generateBasemapResources(assets);
-  // generateGroundResources(assets);
+  generateAPIResources(assets);
+  generateBasemapResources(assets);
+  generateGroundResources(assets);
 
   return assets;
 }
@@ -35,86 +34,87 @@ function generateRootResources(assets: Asset[]) {
   assets.push(`${root}/src/img/esri-10GlobeLogo_1C.png`);
 }
 
-// function generateAPIResources(assets: Asset[]) {
-//   const resources = [
-//     "dojo/dojo.js",
-//     "dojo/nls/dojo_en-us.js",
-//     "esri/views/SceneView.js",
-//     "esri/WebScene.js",
-//     "esri/views/nls/SceneView_en-us.js",
-//     "dojo/resources/blank.gif",
-//     "esri/workers/mutableWorker.js",
-//     "esri/workers/indexWorker.js",
-//     "esri/workers/scripts/helpers.js",
-//     "esri/workers/scripts/indexInterface.js",
-//     "esri/workers/libs/rtree.js",
-//     "esri/geometry/geometryEngine.js",
-//     "esri/layers/GraphicsLayer.js",
-//     "esri/layers/support/ElevationQuery.js",
-//     "esri/portal/support/layersLoader.js",
-//     "esri/views/layers/GroupLayerView.js",
-//     "esri/views/3d/webgl-engine/lib/SmaaRenderPassData.js",
-//     "esri/layers/support/ElevationTile.js",
-//     "esri/views/3d/layers/TileLayerView3D.js",
-//     "esri/css/main.css",
-//     "esri/themes/base/icons/fonts/CalciteWebCoreIcons.ttf?cu4poq",
-//     "esri/themes/base/fonts/avenir-next/Avenir_Next_W00_400.woff2",
-//     "esri/views/3d/environment/resources/stars.wsv"
-//   ];
+function generateAPIResources(assets: Asset[]) {
+  const resources = [
+    "dojo/dojo.js",
+    "dojo/nls/dojo_en-us.js",
+    "esri/views/SceneView.js",
+    "esri/WebScene.js",
+    "esri/views/nls/SceneView_en-us.js",
+    "dojo/resources/blank.gif",
+    "esri/workers/mutableWorker.js",
+    "esri/workers/indexWorker.js",
+    "esri/workers/scripts/helpers.js",
+    "esri/workers/scripts/indexInterface.js",
+    "esri/workers/libs/rtree.js",
+    "esri/geometry/geometryEngine.js",
+    "esri/layers/GraphicsLayer.js",
+    "esri/layers/support/ElevationQuery.js",
+    "esri/portal/support/layersLoader.js",
+    "esri/views/layers/GroupLayerView.js",
+    "esri/views/3d/webgl-engine/lib/SmaaRenderPassData.js",
+    "esri/layers/support/ElevationTile.js",
+    "esri/views/3d/layers/TileLayerView3D.js",
+    "esri/css/main.css",
+    "esri/themes/base/icons/fonts/CalciteWebCoreIcons.ttf?cu4poq",
+    "esri/themes/base/fonts/avenir-next/Avenir_Next_W00_400.woff2",
+    "esri/views/3d/environment/resources/stars.wsv"
+  ];
 
-//   assets.push("https://jsdev.arcgis.com/4.7/");
+  assets.push("https://jsdev.arcgis.com/4.7/");
 
-//   for (const resource of resources) {
-//     assets.push(`https://jsdev.arcgis.com/4.7/${resource}`);
-//   }
-// }
+  for (const resource of resources) {
+    assets.push(`https://jsdev.arcgis.com/4.7/${resource}`);
+  }
+}
 
-const imageryDomain = "services.arcgisonline.com";
+//const imageryDomain = "services.arcgisonline.com";
+const imageryDomain = "wtb.maptiles.arcgis.com";
 
-// function generateTileResources(serverUrl: string, numLevels: number, assets: Asset[]) {
-//   assets.push(`${serverUrl}?f=json`);
+ function generateTileResources(serverUrl: string, numLevels: number, assets: Asset[]) {
+   assets.push(`${serverUrl}?f=json`);
 
-//   for (let i = 0; i < numLevels; i++) {
-//     let n = 1 << i;
+   for (let i = 0; i < numLevels; i++) {
+     const n = 1 << i;
 
-//     assets.push(`${serverUrl}/tilemap/${i}/0/0/32/32`);
+     assets.push(`${serverUrl}/tilemap/${i}/0/0/32/32`);
 
-//     for (let x = 0; x < n; x++) {
-//       for (let y = 0; y < n; y++) {
-//         assets.push(`${serverUrl}/tile/${i}/${x}/${y}`);
-//       }
-//     }
-//   }
-// }
+     for (let x = 0; x < n; x++) {
+       for (let y = 0; y < n; y++) {
+         assets.push(`${serverUrl}/tile/${i}/${x}/${y}`);
+       }
+     }
+   }
+ }
 
-// function generateBasemapResources(assets: Asset[]) {
-//   const serverUrl = `https://${imageryDomain}/ArcGIS/rest/services/World_Imagery/MapServer`;
-//   const cachedLevels = 3;
+ function generateBasemapResources(assets: Asset[]) {
+   const serverUrl = `https://${imageryDomain}/ArcGIS/rest/services/World_Imagery/MapServer`;
+   const cachedLevels = 3;
 
-//   generateTileResources(serverUrl, cachedLevels, assets);
-// }
+   generateTileResources(serverUrl, cachedLevels, assets);
+ }
 
-// function generateGroundResources(assets: Asset[]) {
-//   const serverUrl = "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer";
-//   const cachedLevels = 2;
+ function generateGroundResources(assets: Asset[]) {
+   const serverUrl = "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer";
+   const cachedLevels = 2;
 
-//   generateTileResources(serverUrl, cachedLevels, assets);
-// }
+   generateTileResources(serverUrl, cachedLevels, assets);
+ }
 
-// function generatePortalResources(assets: Asset[]) {
-//   assets.push(
-//     "https://www.arcgis.com/sharing/rest/portals/self?f=json&culture=en-us"
-//   );
-// }
+ function generatePortalResources(assets: Asset[]) {
+   assets.push(
+     "https://www.arcgis.com/sharing/rest/portals/self?f=json&culture=en-us"
+   );
+ }
 
 self.addEventListener("install", (event: any) => {
   console.log("install");
   event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(cache => {
-          const assets = generateAssets();
-          return cache.addAll(assets);
-        })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        const assets = generateAssets();
+        return cache.addAll(assets);
+      })
   );
 });
 
@@ -137,32 +137,32 @@ self.addEventListener("fetch", (event: any) => {
 
   event.respondWith(
     caches.match(req, { ignoreVary: true })
-        .then(response => {
-          if (response) {
-            return response;
-          }
+      .then(response => {
+        if (response) {
+          return response;
+        }
 
-          const fetchRequest = req.clone();
+        const fetchRequest = req.clone();
 
-          return fetch(fetchRequest)
-              .then(response => {
-                if (!response || (response.type !== "opaque" && response.status !== 200) || response.type === "error") {
-                  return response;
-                }
+        return fetch(fetchRequest)
+          .then(response => {
+            if (!response || (response.type !== "opaque" && response.status !== 200) || response.type === "error") {
+              return response;
+            }
 
-                if (req.url.indexOf("chrome-extension:") === 0) {
-                  return response;
-                }
+            if (req.url.indexOf("chrome-extension:") === 0) {
+              return response;
+            }
 
-                const responseToCache = response.clone();
+            const responseToCache = response.clone();
 
-                caches.open(CACHE_NAME)
-                    .then(cache => {
-                      cache.put(req, responseToCache);
-                    });
-
-                return response;
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(req, responseToCache);
               });
-        })
+
+            return response;
+          });
+      })
   );
 });
